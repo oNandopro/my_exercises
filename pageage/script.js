835d8030b9b52290735d149ee379598d917df8eb
oNando <139186322+oNandopro@users.noreply.github.com>
2023-08-23 20:12:04 -0300

    bug corrigido

--- pageage/script.js
+++ pageage/script.js
@@ -3,37 +3,35 @@ function Verificar() {
     var anoatu = data.getFullYear()
     var res = document.getElementById('res')
     var nas = document.getElementById('nasusu')
-    if (nas.value.length == 0 || Number(nas.value) > anoatu) {
-        window.alert=('[ERRO] Ano invalido, verifique os dados inseridos e tente novamente')
+    if ( Number(nas.value) > anoatu) {
+        window.alert('[ERRO] Ano inválido, verifique os dados inseridos e tente novamente')
     } else {
         var sexusu = document.getElementsByName('radsex')
         var idade = anoatu - Number(nas.value)
         var genero = ''
         if (sexusu [0].checked) {
             genero = 'Homem'
-            if (idade >=0 && idade <7) {
+            if (idade >=0 && idade <=7) {
                 img.src= 'imagens/bebe-m.png'
-            } else if (idade <=21) {
+            } else if (idade >7 && idade <=21) {
                 img.src= 'imagens/jovem-m.png'
-            } else if (idade <61) {
+            } else if (idade >21 && idade <61) {
                 img.src= 'imagens/adulto-m.png'
             } else {
                 img.src= 'imagens/idoso-m.png'
             }    
         } else if (sexusu[1].checked){
             genero = 'Mulher'
-            if (idade >=0 && idade <7) {
+            if (idade >=0 && idade <=7) {
                 img.src= 'imagens/bebe-f.png'
-            } else if (idade <=21) {
+            } else if (idade >7 && idade <=21) {
                 img.src= 'imagens/jovem-f.png'
-            } else if (idade <=61) {
+            } else if (idade > 21 && idade <=61) {
                 img.src= 'imagens/adulto-f.png'
             } else {
                 img.src= 'imagens/idoso-f.png'
             }
-
         }
-
-        res.innerHTML=`Indetificamos que você é ${genero} de ${idade} anos!`
+        res.innerHTML=(`Indetificamos que você é ${genero} de ${idade} anos!`)
     }
 }
